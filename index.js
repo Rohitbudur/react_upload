364178b538050694089ee601f0dc4e4d0184d093
Rohitbudur <45158317+Rohitbudur@users.noreply.github.com>
2021-04-18 21:12:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../src/App';
+
+const Appp = () => {
+  return (
+    <App/>
+  );
+};
+
+ReactDOM.render(<Appp />, document.querySelector('#root'));
